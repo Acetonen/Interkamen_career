2865599f7d9d77381e9e9adc42578fcb76d2b60f
Anton Kovalev <acetonen@gmail.com>
2018-12-15 19:29:30 +0300

    Add exel dump for drill passport.

--- modules/drill_passports.py
+++ modules/drill_passports.py
@@ -5,6 +5,8 @@
 import os
 import pandas as pd
 from numpy import nan as Nan
+from openpyxl import load_workbook
+from openpyxl.drawing.image import Image
 from modules.support_modules.standart_functions import BasicFunctions
 from modules.support_modules.absolyte_path_module import AbsolytePath
 
@@ -189,6 +191,16 @@ class DrillPassports(BasicFunctions):
         'pownd_consump', 'pownder', 'detonators', 'd_sh', 'master',
         'bits_in_rock'
     ]
+    months = [
+        '', 'января', 'февраля', 'марта', 'апреля', 'мая', 'июня',
+        'июля', 'августа', 'сентября', 'октября', 'ноября', 'декабря'
+    ]
+    img_path = (AbsolytePath('').get_absolyte_path()[:-5]
+                + 'exl_blancs/scheme.png')
+    blanc_path = (AbsolytePath('').get_absolyte_path()[:-5]
+                  + 'exl_blancs/drill_passport.xlsx')
+    pass_path = (AbsolytePath('').get_absolyte_path()[:-5]
+                 + 'Буровые_паспорта/')
 
     def __init__(self):
         self._create_empty_df()
@@ -218,15 +230,14 @@ class DrillPassports(BasicFunctions):
         """Save passport or not."""
         save = input("\n[c] - сохранить паспорт: ")
         if save in ['c', 'C', 'с', 'С']:
-            pass_name = ("{}-{}-{} {}"
-                         .format(passport.params.year,
-                                 passport.params.month,
-                                 passport.params.day,
-                                 int(passport.params.number)))
+            pass_name = self._create_pass_name(passport)
             print('\033[92m', pass_name, ' - cохранен.\033[0m')
             self.drill_pass_file[pass_name] = passport
             super().dump_data(self.drill_pass_path, self.drill_pass_file)
             super().save_log_to_temp_file(pass_name)
+            exel = input("\nЖелаете создать exel файл? д/н: ")
+            if exel == 'д':
+                self._dump_to_exl(passport)
         else:
             print("Вы отменили сохранение.")
 
@@ -284,3 +295,50 @@ class DrillPassports(BasicFunctions):
                 super().save_log_to_temp_file(passport_name + log)
             else:
                 self._save_or_not(passport)
+
+    @classmethod
+    def _create_pass_name(cls, passport):
+        """Create passport name."""
+        pass_name = ("{}-{}-{} {}"
+                     .format(passport.params.year,
+                             passport.params.month,
+                             passport.params.day,
+                             int(passport.params.number)))
+        return pass_name
+
+    def _dump_to_exl(self, passport):
+        """Dump data to xlsx file."""
+        workbook = load_workbook(self.blanc_path)
+        worksheet = workbook.active
+        img = Image(self.img_path)  # Add image.
+        worksheet.add_image(img, 'A28')
+        worksheet['F1'] = int(passport.params.number)  # Passport number.
+        worksheet['F5'] = str(passport.params.day)  # Day.
+        worksheet['G5'] = self.months[int(passport.params.month)]  # Month.
+        worksheet['H5'] = str(passport.params.year)  # Year.
+        worksheet['K7'] = str(passport.params.horizond)  # Horizond.
+        worksheet['D11'] = float(passport.params.pownder)  # Pownder.
+        worksheet['H11'] = int(passport.params.d_sh)  # D_SH.
+        worksheet['K11'] = int(passport.params.detonators)  # Detonators.
+        # Bareholes.
+        row_number = 17
+        for length in passport.bareholes:
+            worksheet['C' + str(row_number)] = length
+            worksheet['A' + str(row_number)] = int(passport.bareholes[length])
+            row_number += 1
+        # Volume.
+        volume = float(passport.params.pownder) * 5
+        worksheet['F25'] = volume
+        # Block params.
+        height = float(passport.params.block_height)
+        worksheet['H23'] = height
+        depth = float(passport.params.block_depth)
+        worksheet['J23'] = depth
+        worksheet['L23'] = round(volume / height / depth, 1)
+        # Master.
+        master = super().make_name_short(str(passport.params.master))
+        worksheet['G49'] = master
+        # Save file.
+        pass_name = self._create_pass_name(passport)
+        workbook.save(self.pass_path + pass_name + '.xlsx')
+        print("\nФайл сохранен:\n", self.pass_path + pass_name + '.xlsx')
