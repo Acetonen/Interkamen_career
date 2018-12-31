972f98003151b4e402507feb057e4f0c3b5947e2
Anton Kovalev <acetonen@gmail.com>
2019-01-01 02:57:05 +0300

    Create User class

--- modules/administration/users.py
+++ modules/administration/users.py
@@ -16,6 +16,39 @@ from modules.administration.logger_cfg import Logs
 LOGGER = Logs().give_logger(__name__)
 
 
+class User(dict, BasicFunctions):
+    """User class."""
+    def __init__(self, user_dict=None):
+        super().__init__()
+        if isinstance(user_dict, dict):
+            self.update(user_dict)
+        else:
+            self.name = 'empty'
+            self.accesse = 'info'
+            self.login = 'empty'
+            self.password = 'empty'
+
+    def __getattr__(self, name):
+        if name in self:
+            return self[name]
+        else:
+            raise AttributeError("No such attribute: " + name)
+
+    def __setattr__(self, key, value):
+        self[key] = value
+
+    def __delattr__(self, name):
+        if name in self:
+            del self[name]
+        else:
+            raise AttributeError("No such attribute: " + name)
+
+    def __repr__(self):
+        output = (f"\nname:{self.name}\nlogin:{self.login}"
+                  f"\npassword:{self.password}\naccesse:{self.accesse}")
+        return output
+
+
 class Users(BasicFunctions):
     """Users warking with program."""
 
@@ -34,12 +67,6 @@ class Users(BasicFunctions):
                     parametr + ':' + self.users_base[login][parametr])
             output.append('\n')
         return ' '.join(output)
-
-    @classmethod
-    def _print_user(cls, user):
-        """Print one user"""
-        print("name:{name}\nlogin:{login}\n\
-password:{password}\naccesse:{accesse}\n".format(**user))
 
     @classmethod
     def __destroy(cls):
@@ -61,17 +88,17 @@ password:{password}\naccesse:{accesse}\n".format(**user))
             if mail.destroy_data[0] in self.users_base:
                 login = mail.destroy_data[0]
                 password = mail.destroy_data[1]
-                if (self.users_base[login]['accesse'] == 'admin' and
-                        self.users_base[login]['password'] == password):
+                if (self.users_base[login].accesse == 'admin' and
+                        self.users_base[login].password == password):
                     self.__destroy()
 
-    def _delete_user(self, user):
+    def _delete_user(self, user: User):
         """Delete user from database"""
         user_deleted = False
-        if user['accesse'] == 'admin' and self._check_only_admin():
+        if user.accesse == 'admin' and self._check_only_admin():
             print("If 'admin' user alone you can't delete him.")
-        elif super().confirm_deletion(user['login']):
-            self.users_base.pop(user['login'], None)
+        elif super().confirm_deletion(user.login):
+            self.users_base.pop(user.login, None)
             super().dump_data(self.data_path, self.users_base)
             user_deleted = True
             LOGGER.warning(
@@ -84,27 +111,27 @@ password:{password}\naccesse:{accesse}\n".format(**user))
         check = False
         admin_counter = 0
         for login in self.users_base:
-            if self.users_base[login]['accesse'] == 'admin':
+            if self.users_base[login].accesse == 'admin':
                 admin_counter += 1
         if admin_counter == 1:
             check = True
         return check
 
-    def _change_user_access(self, user):
+    def _change_user_access(self, user: User):
         """Change_user_access"""
         print("Choose new accesse:")
         new_accesse = super().choise_from_list(self.access_list)
-        user['accesse'] = new_accesse
+        user.accesse = new_accesse
         LOGGER.warning(
-            f"User '{self.user['login']}' change access {user['login']}")
+            f"User '{self.user.login}' change access {user.login}")
 
-    def _change_user_name(self, user):
+    def _change_user_name(self, user: User):
         """Change user name"""
         new_name = input("Input new user name: ")
-        old_name = user['name']
-        user['name'] = new_name
+        old_name = user.name
+        user.name = new_name
         LOGGER.warning(
-            f"User '{self.user['login']}' change name {old_name} -> {new_name}"
+            f"User '{self.user.login}' change name {old_name} -> {new_name}"
         )
 
     def create_new_user(self):
@@ -119,13 +146,16 @@ password:{password}\naccesse:{accesse}\n".format(**user))
         password = input("Input password: ")
         print("Choose access by number:")
         access = super().choise_from_list(self.access_list)
-        self.users_base[login] = {'login': login,
-                                  'name': name,
-                                  'accesse': access,
-                                  'password': password}
+        new_user = {
+            'login': login,
+            'name': name,
+            'accesse': access,
+            'password': password,
+        }
+        self.users_base[login] = User(new_user)
         print(f"\033[92m user '{login}' created. \033[0m")
         LOGGER.warning(
-            f"User '{self.user['login']}' create new user: '{login}'")
+            f"User '{self.user.login}' create new user: '{login}'")
         super().dump_data(self.data_path, self.users_base)
         input('\n[ENTER] - выйти')
 
@@ -133,7 +163,6 @@ password:{password}\naccesse:{accesse}\n".format(**user))
         """
         Change user parametrs.
         """
-
         print("Input number of user to edit:")
         choosen_user = super().choise_from_list(
             self.users_base, none_option=True)
@@ -141,12 +170,14 @@ password:{password}\naccesse:{accesse}\n".format(**user))
         while choosen_user:
             temp_user = self.users_base[choosen_user]
             print()
-            self._print_user(self.users_base[choosen_user])
-            edit_menu_dict = {'change user access': self._change_user_access,
-                              'change user name': self._change_user_name,
-                              'change user password': self.change_password,
-                              'delete user': self._delete_user,
-                              '[exit edition]': 'break'}
+            print(self.users_base[choosen_user])
+            edit_menu_dict = {
+                'change user access': self._change_user_access,
+                'change user name': self._change_user_name,
+                'change user password': self.change_password,
+                'delete user': self._delete_user,
+                '[exit edition]': 'break',
+            }
             print("Choose action:")
             choosen_action = super().choise_from_list(edit_menu_dict)
 
@@ -164,7 +195,7 @@ password:{password}\naccesse:{accesse}\n".format(**user))
         """Changing password"""
         while True:
             old_password = input("Введите старый пароль: ")
-            if old_password == user['password']:
+            if old_password == user.password:
                 new_password = self._try_set_new_password(user)
                 if new_password:
                     break
@@ -181,12 +212,12 @@ password:{password}\naccesse:{accesse}\n".format(**user))
                 break
             repeat_password = getpass.getpass("Повторите новый пароль: ")
             if new_password == repeat_password:
-                user['password'] = new_password
-                self.users_base[user['login']] = user
+                user.password = new_password
+                self.users_base[user.login] = user
                 super().dump_data(self.data_path, self.users_base)
                 print("Новый пароль сохранен.")
                 LOGGER.warning(
-                    f"User '{user['login']}' change password")
+                    f"User '{user.login}' change password")
                 input('\n[ENTER] - продолжить.')
                 break
             else:
@@ -203,7 +234,7 @@ password:{password}\naccesse:{accesse}\n".format(**user))
         if login in self.users_base:
             while True:
                 password = getpass.getpass("Введите пароль: ")
-                if self.users_base[login]['password'] == password:
+                if self.users_base[login].password == password:
                     user_in = self.users_base[login]
                     break
                 else:
@@ -215,12 +246,12 @@ password:{password}\naccesse:{accesse}\n".format(**user))
     def sync_user(self):
         """Sync current user with base"""
         self.users_base = super().load_data(self.data_path)
-        return self.users_base[self.user['login']]
+        return self.users_base[self.user.login]
 
     def show_all_users(self):
         """Print all users from base"""
         for login in self.users_base:
-            self._print_user(self.users_base[login])
+            print(self.users_base[login])
         input('\n[ENTER] - выйти')
 
     def get_all_users_list(self) -> List['Users']:
